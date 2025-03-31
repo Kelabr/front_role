cd8b2a19d45403b5dbb0e8a5939ee813fc8a63d1
Kelabr <kelabrantes.me@gmail.com>
2025-03-30 21:38:12 -0300

    logica de envio de formulário do login

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -1,15 +1,49 @@
+"use client"
+
 export default function LoginForm(){
+
+    async function handleSubmit(event){
+        event.preventDefault()
+
+        const formData = new FormData(event.target)
+
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+
+        try{
+            const response = await fetch("http://localhost:3333/api/user", {
+                method:"POST",
+                headers:{
+                     "Content-Type": "application/json"
+                },
+                body: JSON.stringify({email, password}),
+                credentials:"include"
+            })
+
+            if(!response.ok){
+                throw new Error("Erro ao fazer login")
+            }
+        }catch(error){
+            console.error("Erro ao logar", error)
+        }
+
+        console.log("Logado")
+
+    }
+
+
     return(
-        <form className="flex flex-col gap-6">
+        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-1">
                 <label className="font-semibold text-lg">Email</label>
-                <input type="email" className="border-2 p-2 rounded-xl" />
+                <input type="email" className="border-2 p-2 rounded-xl" name="email" />
             </div>
             <div className="flex flex-col gap-1">
                 <label className="font-semibold text-lg">Senha</label>
-                <input type="password" className="border-2 p-2 rounded-xl" />
+                <input type="password" className="border-2 p-2 rounded-xl" name="password" />
             </div>
-            <button className="bg-black text-white font-medium p-3 text-lg rounded-2xl">Entrar</button>
+            <button className="bg-black text-white font-medium p-3 text-lg rounded-2xl" type="submit">Entrar</button>
         </form>
     )
 }
